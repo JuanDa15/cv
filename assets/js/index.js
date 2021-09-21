03ebf663e72efb5004ec79c1093ab7303cd9403f
Juan David <jdoo1115@gmail.com>
2021-09-21 10:28:27 -0500

    Navbar styles, missing theaming and other details

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,14 @@
 const btn_switch = document.querySelector('#switch');
 
+const btn_toggle = document.querySelector('#toggle');
+
+const links = document.querySelector('.links');
+
 btn_switch.addEventListener('click', ()=>{
   document.body.classList.toggle('dark');
   btn_switch.classList.toggle('active');
+})
+
+btn_toggle.addEventListener('click', ()=>{
+  links.classList.toggle('hide');
 })
