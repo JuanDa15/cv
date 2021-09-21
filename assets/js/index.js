dca217367569570880e7bfce70d7dff33cfb4e34
Juan David <jdoo1115@gmail.com>
2021-09-21 11:14:21 -0500

    responsive nav fixed, animations added

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,8 @@ const btn_toggle = document.querySelector('#toggle');
 
 const links = document.querySelector('.links');
 
+const icon = document.querySelector('#toggle i');
+
 btn_switch.addEventListener('click', ()=>{
   document.body.classList.toggle('dark');
   btn_switch.classList.toggle('active');
@@ -11,4 +13,13 @@ btn_switch.addEventListener('click', ()=>{
 
 btn_toggle.addEventListener('click', ()=>{
   links.classList.toggle('hide');
+  btn_toggle.classList.toggle('toggle_animation');
+
+  if(icon.classList.contains('fa-bars')){
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-times');
+  }else{
+    icon.classList.remove('fa-times');
+    icon.classList.add('fa-bars');
+  }
 })
